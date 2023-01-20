6f48300425d057045607c0629a8cf5cf5cf100af
Pr1mat1k <123157494+Pr1mat1k@users.noreply.github.com>
2023-01-20 13:11:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,59 +1,58 @@
 import unittest
 import Number1
 
-class testTypeTriangle(unittest.TestCase):
+
+class TestTypeTriangle(unittest.TestCase):
 
     # Равносторонний
     def test_equilateralTriange(self):
+        # Тестирование разлчиных вариантов значений
 
-        #Тестирование разлчиных вариантов значений
-
-        #Тестирование на корректный ввод
+        # Тестирование на корректный ввод
         self.assertTrue(Number1.equilateralTriange(12, 12, 12), True)
 
-         #Тестирование на некорректный ввод данных
+        # Тестирование на некорректный ввод данных
         self.assertFalse(Number1.equilateralTriange(16, 16, 0), False)
         self.assertFalse(Number1.equilateralTriange(7, 0, 7), False)
         self.assertFalse(Number1.equilateralTriange(0, 1, 1), False)
 
-        #Эквивалентное
-        #Тестирование целочисленные
+        # Эквивалентное
+        # Тестирование целочисленные
         self.assertFalse(Number1.equilateralTriange(-5, 0, 15), False)
-        
-        #Тестирование специального ввода специального символа
+
+        # Тестирование специального ввода специального символа
         self.assertFalse(Number1.equilateralTriange('@', 12, 13), False)
 
-        #Тестирование ввода символов
+        # Тестирование ввода символов
         self.assertFalse(Number1.equilateralTriange('a', 'b', 'c'), False)
 
-        #Граничное
+        # Граничное
         # Граничное нижней границы и верхней
-        self.assertFalse(Number1.equilateralTriange(-2147483648, 0, 2147483647), False) 
+        self.assertFalse(Number1.equilateralTriange(-2147483648, 0, 2147483647), False)
 
-        #Тестирование вернхней границы
+        # Тестирование вернхней границы
         self.assertFalse(Number1.equilateralTriange(2147483646, 2147483647, 2147483648), False)
 
-        #Тестирование нижней гранциы
+        # Тестирование нижней гранциы
         self.assertFalse(Number1.equilateralTriange(-21474836468, -2147483647, -2147483646), False)
 
     # Разносторонний
     def test_versatileTriangle(self):
-
-        #Тестирование на корректный ввод
+        # Тестирование на корректный ввод
         self.assertTrue(Number1.versatileTriangle(13, 10, 11), True)
 
-        #Тестирование на некорректный ввод данных
+        # Тестирование на некорректный ввод данных
         self.assertFalse(Number1.versatileTriangle(12, 12, 0), False)
         self.assertFalse(Number1.versatileTriangle(0, 19, 19), False)
 
         # Эквивалентное по типу значения отрицательноое,
-        #Тестирование целочисленные
+        # Тестирование целочисленные
         self.assertTrue(Number1.versatileTriangle(-1, 0, 1), False)
 
-         #Тестирование специального ввода специального символа
+        # Тестирование специального ввода специального символа
         self.assertTrue(Number1.versatileTriangle('@', 12, 13), False)
 
-        #Тестирование ввода символов
+        # Тестирование ввода символов
         self.assertTrue(Number1.versatileTriangle('a', 'b', 'c'), False)
 
         # Граничное
@@ -66,7 +65,6 @@ class testTypeTriangle(unittest.TestCase):
 
     # Равнобедренный
     def test_isoscelesTriangle(self):
-
         # Тестирование на корркетный ввод
         self.assertTrue(Number1.isoscelesTriangle(12, 12, 6), True)
         self.assertTrue(Number1.isoscelesTriangle(15, 9, 15), True)
@@ -81,23 +79,22 @@ class testTypeTriangle(unittest.TestCase):
         self.assertFalse(Number1.isoscelesTriangle(22, 21, 16), False)
 
         # Эквивалентное 
-        #Тестирование целочисленные
+        # Тестирование целочисленные
         self.assertFalse(Number1.isoscelesTriangle(-5, 0, 15), False)
-        #Тестирование специального ввода специального символа
+        # Тестирование специального ввода специального символа
         self.assertFalse(Number1.isoscelesTriangle('@', 12, 13), False)
-        #Тестирование ввода символов
+        # Тестирование ввода символов
         self.assertFalse(Number1.isoscelesTriangle('a', 'b', 'c'), False)
 
         # Граничное
         # Граничное нижней границы до верхней
         self.assertFalse(Number1.isoscelesTriangle(-2147483648, 0, 2147483647), False)
-        #Тестирование верхней гранциы
-        #Тестирование нижней гранциы
+        # Тестирование верхней гранциы
+        # Тестирование нижней гранциы
         self.assertFalse(Number1.isoscelesTriangle(-21474836468, -2147483647, -2147483646), False)
 
-
     def test_checkNumber(self):
-        #Тестирование на корркетный ввод данных
+        # Тестирование на корркетный ввод данных
         self.assertTrue(Number1.checkNumber(1, 2, 3), True)
         self.assertTrue(Number1.checkNumber(1, 3, 2), True)
         self.assertTrue(Number1.checkNumber(3, 1, 2), True)
@@ -105,15 +102,14 @@ class testTypeTriangle(unittest.TestCase):
         self.assertTrue(Number1.checkNumber(2, 1, 3), True)
         self.assertTrue(Number1.checkNumber(2, 3, 1), True)
 
-        #Тестирование на некорректный ввод данных
+        # Тестирование на некорректный ввод данных
         self.assertFalse(Number1.checkNumber(3, 1, 0), False)
         self.assertFalse(Number1.checkNumber(4, 0, 2), False)
         self.assertFalse(Number1.checkNumber(0, 3, 1), False)
 
         # Эквивалентное 
-        #Тестирование целочисленные
+        # Тестирование целочисленные
         self.assertFalse(Number1.checkNumber(-5, 0, 15), False)
-
 
         # Граничное
         # Граничное нижней границы до верхней
