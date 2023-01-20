ce748a3c504263a0b3672c5257cc80b847606bdd
Pr1mat1k <123157494+Pr1mat1k@users.noreply.github.com>
2023-01-20 13:09:01 +0300

    Update Number1.py

--- Number1.py
+++ Number1.py
@@ -1,13 +1,13 @@
 from math import sqrt
 
 
-# def triangleType(a, b, c):
-#     if a == b == c:
-#         return "Треугольник равносторонний"
-#     elif a == b and b != c or a == c and a != b or b == c and b != a:
-#         return "Треугольник равнобедренный"
-#     else:
-#         return "Треугольник разносторонний"
+def triangleType(a, b, c):
+    if a == b == c:
+        return "Треугольник равносторонний"
+    elif a == b and b != c or a == c and a != b or b == c and b != a:
+        return "Треугольник равнобедренный"
+    else:
+        return "Треугольник разносторонний"
 
 #Равносторонний
 def equilateralTriange(a, b, c):
@@ -16,43 +16,44 @@ def equilateralTriange(a, b, c):
     else:
         return False
 
-#Равнобедренный
+
+# Равнобедренный
 def isoscelesTriangle(a, b, c):
     if a == b and b != c or a == c and a != b or b == c and b != a:
         return True
     else:
         return False
 
-#Разносторонний
+
+# Разносторонний
 def versatileTriangle(a, b, c):
-    if (a != b) and  (b != c):
+    if (a != b) and (b != c):
         return True
     else:
         return False
 
-#Проверка условия
-def checkNumber(a,b,c):
+
+# Проверка условия
+def checkNumber(a, b, c):
     status = bool(False)
-    if (a+b >= c and a+c >= b and b+c >= a) and (a > 0 and b > 0 and c > 0):
+    if (a + b >= c and a + c >= b and b + c >= a) and (a > 0 and b > 0 and c > 0):
         status = True
         return status
     else:
         return status
 
+
+# Функция вычисления площади треугольника по формуле Герона
 def getSquare(a, b, c):
     p = (a + b + c) / 2
     square = sqrt(p * (p - a) * (p - b) * (p - c))
     return square
 
-print('Введите стороны треугольника: ')
-a, b, c = map(int, input().split())
-if checkNumber(a, b, c):
-    #print(triangleType(a, b, c))
-    print(f'S = {getSquare(a, b, c)}')
-    print(equilateralTriange(a, b, c))
-    print(isoscelesTriangle(a, b, c))
-    print(versatileTriangle(a, b, c))
-else:
-    print('Ошибка')
+
+def main():
+    print('Введите стороны треугольника: ')
+    a, b, c = map(int, input().split())
 
 
+if __name__ == "__main__":
+    main()
